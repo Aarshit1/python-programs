f503b7541101a0c7c8b706777c0b60ff7acb8604
Aarshit1 <aarshit.khanal@gmail.com>
2026-05-07 20:10:24 +0545

    Add files via upload

---
+++ nested_list1.py
@@ -0,0 +1,15 @@
+i=int(input("Enter a number between 10 and 20 : "))
+
+if i==13:
+    if i<15:
+        print("i is less than 15")
+    if i<13:
+        print("i is less than 13 too")
+    else:
+        print("i is greater than 12 and less than 15")
+
+elif i>=11 :
+    if i<15:
+        print("i is less than 15")
+    else:
+        print("i is greater than 11 and less than 15")
