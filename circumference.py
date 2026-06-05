d7ae156db6339555fab88fe21abe633c92a160ad
Aarshit1 <aarshit.khanal@gmail.com>
2026-06-05 20:34:12 +0545

    circles

---
+++ circumference.py
@@ -0,0 +1,7 @@
+def circumCalculator():
+    diameter=float(input("enter the diameter of your cirlce : "))
+    radius=diameter/2
+    circumference=2*3.14*radius
+    print("circumference is ",circumference)
+
+circumCalculator()
