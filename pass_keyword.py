74cfb42e227d0b3cb2be79ffadc224e160e0dd10
Aarshit1 <aarshit.khanal@gmail.com>
2026-06-12 19:40:59 +0545

    ez

---
+++ pass_keyword.py
@@ -0,0 +1,13 @@
+a=int(input("enter a number : "))
+
+for i in range (a):
+    if(i%20==0):
+        print("twist")
+    elif(i%15==0):
+        pass
+    elif(i%5==0):
+        print("fizz")
+    elif(i%3==0):
+        print("buzz")
+    else:
+        print("number isn't divisible")
