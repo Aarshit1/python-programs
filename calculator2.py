4d381048c9d0750c334cc5a29b672f0d3edd7067
Aarshit1 <aarshit.khanal@gmail.com>
2026-06-12 19:47:05 +0545

    calc2

---
+++ calculator2.py
@@ -0,0 +1,16 @@
+def add(a,b):
+    return(a+b)
+
+def subtract(a,b):
+    return(a-b)
+
+def multiply(a,b):
+    return(a*b)
+
+def divide(a,b):
+    return(a/b)
+
+print(add(5,4))
+print(subtract(3,2))
+print(multiply(7,8))
+print(divide(9,3))
