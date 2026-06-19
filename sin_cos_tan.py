0de37aaffa2dcd8879ada5f606854829b470a058
Aarshit1 <aarshit.khanal@gmail.com>
2026-06-19 20:03:29 +0545

    sincostan

---
+++ sin_cos_tan.py
@@ -0,0 +1,11 @@
+import math
+
+num=int(input("enter a number : "))
+
+sin=math.sin(num)
+cos=math.cos(num)
+tan=math.tan(num)
+
+print("\nthe sin value of ",num,"is ",sin)
+print("\nthe cos value of ",num,"is ",cos)
+print("\nthe tan value of ",num,"is ",tan)
