a2754f8f16ad522c0543b4aeb9bbd8b64b1f7899
Aarshit1 <aarshit.khanal@gmail.com>
2026-07-04 20:05:54 +0545

    datas

---
+++ set2.py
@@ -0,0 +1,13 @@
+a={1,2,3,4,5}
+b={4,5,6,7,8}
+
+c=a.intersection(b)
+d=a.union(b)
+e=a.difference(b)
+f=a.symmetric_difference(b)
+
+print("sets : ",a,b)
+print("intersection : ",c)
+print("union : ",d)
+print("difference : ",e)
+print("symmetric_difference : ",f)
---
+++ set_intersection.py
@@ -0,0 +1,8 @@
+set_a={"red","blue"}
+set_b={"blue","yellow"}
+
+print("original set : ", set_a, set_b)
+
+set_c=set_a.intersection(set_b)
+
+print("\n set after intersection : ", set_c)
